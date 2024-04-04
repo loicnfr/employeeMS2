373956186163f618289e0f30637e93cd5a943924
Percy237 <percylarson17@gmail.com>
2024-04-04 09:00:45 +0100

    implement dashboard ui

--- employeems/src/Dashboard.jsx
+++ employeems/src/Dashboard.jsx
@@ -1,20 +1,60 @@
-import { Card, Space, Statistic, Typography } from "antd";
-import React from "react";
-import { ShoppingCartOutlined } from "@ant-design/icons/lib/icons";
+import { FaUsers } from "react-icons/fa";
+import StatisticCard from "./components/StatisticCard";
 
 const Dashboard = () => {
   return (
-    <>
-      <Typography.Title level={4}>Dashboard</Typography.Title>
-      <Space direction="horizontal">
-        <Card>
-          <Space direction="horizontal">
-            <ShoppingCartOutlined />
-            <Statistic title="Orders" value={1234} />
-          </Space>
-        </Card>
-      </Space>
-    </>
+    <div className="pl-20">
+      <h1 className=" text-3xl mt-1">Dashboard</h1>
+      <div className="grid grid-cols-3 mt-4 gap-20 ">
+        <StatisticCard
+          text="Registered Employees"
+          total="5"
+          iconBgColor="bg-green-600"
+          icon={<FaUsers />}
+        />
+        <StatisticCard
+          text="Listed Departments"
+          total="5"
+          iconBgColor="bg-yellow-400"
+          icon={<FaUsers />}
+        />
+        <StatisticCard
+          text="Listed Leave Type"
+          total="5"
+          iconBgColor="bg-red-600"
+          icon={<FaUsers />}
+        />
+      </div>
+      <div className="flex h-[70px] mt-10 justify-center items-center border-t-2 border-b-2">
+        <h1>Leaves Details</h1>
+      </div>
+      <div className="grid grid-cols-2 gap-8 pt-8">
+        <StatisticCard
+          text="Leaves Applied"
+          total="5"
+          iconBgColor="bg-blue-600"
+          icon={<FaUsers />}
+        />
+        <StatisticCard
+          text="New Leave Requests"
+          total="5"
+          iconBgColor="bg-yellow-400"
+          icon={<FaUsers />}
+        />
+        <StatisticCard
+          text="RejectedLeave Requests"
+          total="5"
+          iconBgColor="bg-red-600"
+          icon={<FaUsers />}
+        />
+        <StatisticCard
+          text="Approved Leave Requests"
+          total="5"
+          iconBgColor="bg-green-600"
+          icon={<FaUsers />}
+        />
+      </div>
+    </div>
   );
 };
 
